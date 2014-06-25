34448c0844254d02bc182e1cca627b688e545a08
Emery Denuccio <emery.denuccio@gmail.com>
2014-06-25 00:23:03 -0700

    init

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Gsim
+
+Cross-platform command-line gcode simulator.
+
+```
+cat smile.gcode | gsim
+```
+
+![alt](test/screeny.png)
+
+
+## Installation
+```
+npm install -g gsim
+```
---
+++ test/test.utils.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+  , utils = require('../lib/utils');
+
+describe('Simulator', function() {
+  it('#arcToPoints', function() {
+    expect(result.end.y).closeTo(5, 0.000001);
+  });
+});
